70052c7eac9f3fd85b24bfdf22c1718f8376a62b
jhunderdog <33382water@gmail.com>
2021-11-09 16:56:04 +0900

    MarshalText

--- main.go
+++ main.go
@@ -9,27 +9,43 @@ import (
 
 const port string = ":4000"
 
+type URL string
+
+func (u URL) MarshalText() ([]byte, error) {
+	url := fmt.Sprintf("http://localhost%s%s", port, u)
+	return []byte(url), nil
+}
+
+// func (u URL) String() string {
+// 	return "hi"
+// }
+
 type URLDescription struct {
-	URL         string `json:"url"`
+	URL         URL    `json:"url"`
 	Method      string `json:"method"`
 	Description string `json:"description`
 	Payload     string `json:"payload,omitempty"`
 }
 
+// func (u URLDescription) String() string {
+// 	return "Hello I'm the URL Description"
+// }
+
 func documentation(rw http.ResponseWriter, r *http.Request) {
 	data := []URLDescription{
 		{
-			URL:         "/",
+			URL:         URL("/"),
 			Method:      "GET",
 			Description: "See Documentation",
 		},
 		{
-			URL:         "/blocks",
+			URL:         URL("/blocks"),
 			Method:      "POST",
 			Description: "Add A Block",
 			Payload:     "data:string",
 		},
 	}
+	// fmt.Println(data)
 	rw.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(rw).Encode(data)
 	// b, err := json.Marshal(data)
@@ -38,6 +54,7 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
 	http.HandleFunc("/", documentation)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
