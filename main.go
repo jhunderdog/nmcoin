1864eb3c66041ebafc816eb5e8fe2b1e73e05dd8
jhunderdog <33382water@gmail.com>
2021-11-09 14:49:40 +0900

    Marshal and Filed Tags

--- main.go
+++ main.go
@@ -1,9 +1,44 @@
 package main
 
 import (
-	"github.com/jhunderdog/nmcoin/explorer"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 )
 
+const port string = ":4000"
+
+type URLDescription struct {
+	URL         string `json:"url"`
+	Method      string `json:"method"`
+	Description string `json:"description`
+	Payload     string `json:"payload,omitempty"`
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+		{
+			URL:         "/blocks",
+			Method:      "POST",
+			Description: "Add A Block",
+			Payload:     "data:string",
+		},
+	}
+	rw.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(data)
+	// b, err := json.Marshal(data)
+	// utils.HandleErr(err)
+	// fmt.Fprintf(rw, "%s", b)
+}
+
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
