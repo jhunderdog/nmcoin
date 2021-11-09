e91ae770dcdeb486f1d0926982b0ea7a223c533d
jhunderdog <33382water@gmail.com>
2021-11-09 19:53:45 +0900

    New ServeMux

--- main.go
+++ main.go
@@ -1,85 +1,12 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/jhunderdog/nmcoin/blockchain"
-	"github.com/jhunderdog/nmcoin/utils"
+	"github.com/jhunderdog/nmcoin/explorer"
+	"github.com/jhunderdog/nmcoin/rest"
 )
 
-const port string = ":4000"
-
-type URL string
-
-func (u URL) MarshalText() ([]byte, error) {
-	url := fmt.Sprintf("http://localhost%s%s", port, u)
-	return []byte(url), nil
-}
-
-// func (u URL) String() string {
-// 	return "hi"
-// }
-
-type URLDescription struct {
-	URL         URL    `json:"url"`
-	Method      string `json:"method"`
-	Description string `json:"description"`
-	Payload     string `json:"payload,omitempty"`
-}
-
-type AddBlockBody struct {
-	Message string
-}
-
-// func (u URLDescription) String() string {
-// 	return "Hello I'm the URL Description"
-// }
-
-func documentation(rw http.ResponseWriter, r *http.Request) {
-	data := []URLDescription{
-		{
-			URL:         URL("/"),
-			Method:      "GET",
-			Description: "See Documentation",
-		},
-		{
-			URL:         URL("/blocks"),
-			Method:      "POST",
-			Description: "Add A Block",
-			Payload:     "data:string",
-		},
-	}
-	// fmt.Println(data)
-	rw.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(rw).Encode(data)
-	// b, err := json.Marshal(data)
-	// utils.HandleErr(err)
-	// fmt.Fprintf(rw, "%s", b)
-}
-
-func blocks(rw http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		rw.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
-	case "POST":
-		// {"data": "my block data"}
-		var addBlockBody AddBlockBody
-		// fmt.Println(addBlockBody)
-		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
-		// fmt.Println(addBlockBody)
-		blockchain.GetBlockchain().AddBlock(addBlockBody.Message)
-		rw.WriteHeader(http.StatusCreated)
-	}
-}
-
 func main() {
+	go explorer.Start(3000)
+	rest.Start(5000)
 
-	http.HandleFunc("/", documentation)
-	http.HandleFunc("/blocks", blocks)
-	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
 }
