8e893ac6ae8994a1c58b46d3ccab8e1da10f579d
jhunderdog <33382water@gmail.com>
2021-11-15 17:29:10 +0900

    Flag

--- main.go
+++ main.go
@@ -1,40 +1,29 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-)
-
-func usage() {
-	fmt.Printf("Welocme to 노마드 코인\n\n")
-	fmt.Printf("Please use the following commands:\n\n")
-	fmt.Printf("explorer:	Start the HTMl Explorer\n")
-	fmt.Printf("rest:	Starts the REST API(recommended)\n\n")
-	os.Exit(0)
-}
+import "github.com/jhunderdog/nmcoin/cli"
 
 func main() {
 	// go explorer.Start(3000)
 	// rest.Start(4000)
-	fmt.Println(os.Args)
-	if len(os.Args) < 2 {
-		usage()
-	}
+	// fmt.Println(os.Args)
+	// if len(os.Args) < 2 {
+	// 	usage()
+	// }
 
-	rest := flag.NewFlagSet("rest", flag.ExitOnError)
-	portFlag := rest.Int("port", 4000, "Sets the port of the server")
+	// rest := flag.NewFlagSet("rest", flag.ExitOnError)
+	// portFlag := rest.Int("port", 4000, "Sets the port of the server")
 
-	switch os.Args[1] {
-	case "explorer":
-		fmt.Println("Start Explorere")
-	case "rest":
-		rest.Parse(os.Args[2:])
-	default:
-		usage()
-	}
-	if rest.Parsed() {
-		fmt.Println(portFlag)
-		fmt.Println("Start server")
-	}
+	// switch os.Args[1] {
+	// case "explorer":
+	// 	fmt.Println("Start Explorere")
+	// case "rest":
+	// 	rest.Parse(os.Args[2:])
+	// default:
+	// 	usage()
+	// }
+	// if rest.Parsed() {
+	// 	fmt.Println(portFlag)
+	// 	fmt.Println("Start server")
+	// }
+	cli.Start()
 }
