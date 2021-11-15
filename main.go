f105d8814ee117daa7628cbf72a0a1783ad2fdab
jhunderdog <33382water@gmail.com>
2021-11-15 16:48:36 +0900

    FlagSet

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
@@ -20,13 +21,20 @@ func main() {
 	if len(os.Args) < 2 {
 		usage()
 	}
+
+	rest := flag.NewFlagSet("rest", flag.ExitOnError)
+	portFlag := rest.Int("port", 4000, "Sets the port of the server")
+
 	switch os.Args[1] {
 	case "explorer":
 		fmt.Println("Start Explorere")
 	case "rest":
-		fmt.Println("Start Rest API")
+		rest.Parse(os.Args[2:])
 	default:
 		usage()
 	}
-
+	if rest.Parsed() {
+		fmt.Println(portFlag)
+		fmt.Println("Start server")
+	}
 }
