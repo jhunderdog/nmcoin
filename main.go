5578df88c37fc890416ed66af76ac2050bccaa8b
jhunderdog <33382water@gmail.com>
2021-11-25 17:09:53 +0900

    Restoring the blockchain

--- main.go
+++ main.go
@@ -5,30 +5,6 @@ import (
 )
 
 func main() {
-	// go explorer.Start(3000)
-	// rest.Start(4000)
-	// fmt.Println(os.Args)
-	// if len(os.Args) < 2 {
-	// 	usage()
-	// }
+	blockchain.Blockchain()
 
-	// rest := flag.NewFlagSet("rest", flag.ExitOnError)
-	// portFlag := rest.Int("port", 4000, "Sets the port of the server")
-
-	// switch os.Args[1] {
-	// case "explorer":
-	// 	fmt.Println("Start Explorere")
-	// case "rest":
-	// 	rest.Parse(os.Args[2:])
-	// default:
-	// 	usage()
-	// }
-	// if rest.Parsed() {
-	// 	fmt.Println(portFlag)
-	// 	fmt.Println("Start server")
-	// }
-	// cli.Start()
-	blockchain.Blockchain().AddBlock("First")
-	blockchain.Blockchain().AddBlock("Second")
-	blockchain.Blockchain().AddBlock("Third")
 }
